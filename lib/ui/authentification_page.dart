cf357086551d0cad7000077cb743940d7c0d4360
dimitri <devgtfod@gmail.com>
2024-03-27 17:40:52 +0000

    authen

---
+++ lib/ui/authentification_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AuthenPage extends StatefulWidget {
+  const AuthenPage({super.key});
+
+  @override
+  State<AuthenPage> createState() => _AuthenPageState();
+}
+
+class _AuthenPageState extends State<AuthenPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
